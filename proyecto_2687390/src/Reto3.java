430462b12b989fb995ccf0fdea55175a3ed2e46c
macoronado <miguelangelcoronadog@gmail.com>
2023-02-17 11:33:17 -0500

    Se creo una clase capaz de sacar un promedio de notas y verificar si se aprueba o no

--- proyecto_2687390/src/Reto3.java
+++ proyecto_2687390/src/Reto3.java
@@ -11,18 +11,42 @@ public class Reto3 {
         int apuesta;
         int numero = aleatorio.nextInt(3);
 
-        System.out.println("ingrese el numero 1 o 2 segun el numero al que quiera apostar");
+        System.out.println("ingrese el numero 1(cara) o 2 (sello) segun el numero al que quiera apostar");
 
         apuesta = lectura.nextInt();
 
-        System.out.println("El numero aleatorio es: " + numero);
+        
 
-        if(apuesta == numero){
-            System.out.println("Ganaste");
-        }else{
-            System.out.println("Perdiste");
+        if (apuesta == 1 ){
+            System.out.println("Escogiste cara");
+        }else if(apuesta == 2){
+            System.out.println("Escogiste sello");
+        }
+
+        if(numero == 1){
+            System.out.println("Salio cara");
+        }else if(numero == 2){
+            System.out.println("Salio sello");
         }
 
 
+       /* if(numero == 1 && apuesta == 1){
+            System.out.println("Ganaste, escogiste sello");
+        }else if(numero == 2 && apuesta == 2){
+            System.out.println("Ganaste, escogiste cara");
+        }else if( numero == 1 && apuesta == 2){
+            System.out.println("Perdiste, escogiste sello");
+        }else if( numero == 2 && apuesta == 1){
+            System.out.println("Perdiste, escogiste cara");
+        } */
+
+
+
+
+
+    
+        
+     lectura.close();
+     
     }
 }
